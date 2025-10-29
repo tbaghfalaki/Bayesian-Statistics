368c6c9bb81c086e827e1b42421bc0b2d3d5d9d3
Taban Baghfalaki <64243070+tbaghfalaki@users.noreply.github.com>
2025-10-29 10:17:44 +0000

    Add files via upload

---
+++ Chapter1/C1.R
@@ -0,0 +1,78 @@
+rm(list=ls())
+library(R2jags)
+# Load packages
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(patchwork)  # for combining plots
+
+# Load data
+Data <- read.csv("C:\\Users\\p80744tb\\Desktop\\titanic\\train_complete.csv", stringsAsFactors = FALSE)
+names(Data)
+attach(Data)
+head(Data)
+
+ 
+
+# Create design matrix (includes intercept)
+X <- model.matrix(~ Sex + Age + as.factor(Pclass) + Fare + 
+                    as.factor(Embarked) + SibSp + Parch, data = Data)
+head(X)
+ 
+
+K <- ncol(X)
+
+
+# Prepare data list for JAGS
+data_jags <- list(
+  N = nrow(X),
+  K = K,
+  Survived = Data$Survived,
+  X = X
+)
+
+
+# Initial values function
+inits_fun <- function() {
+  list(beta = rep(0, K))   # initialize all betas to 0
+}
+
+
+# Parameters to monitor
+params <- c("beta")
+
+
+# JAGS model as a text string
+logistic_model <- "
+model {
+  # Likelihood
+  for (i in 1:N) {
+    Survived[i] ~ dbern(pi[i])
+    logit(pi[i]) <- inprod(beta[], X[i, ])
+  }
+
+  # Priors
+  for (k in 1:K) {
+    beta[k] ~ dnorm(0, 0.001)
+  }
+}
+"
+
+
+# Fit the model using R2jags
+fit <- jags(
+  data = data_jags,
+  inits = inits_fun,
+  parameters.to.save = params,
+  model.file = textConnection(logistic_model),
+  n.chains = 3,
+  n.iter = 10000,
+  n.burnin = 2000
+)
+
+
+# Summarize results
+print(fit)
+
+
+
